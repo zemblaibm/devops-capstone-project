30bcf15234a51c998c0422f53e745e23110fcd6c
zemblaibm <zemblaibm@gmail.com>
2023-08-12 10:54:13 -0400

    5th commit (delete)

--- service/routes.py
+++ service/routes.py
@@ -95,14 +95,36 @@ def create_accounts():
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+    def update_accounts(account_id):
+        """
+        Update an Account
+        This endpoint will update an Account based on the posted data
+        """
+        app.logger.info("Request to update an Account with id: %s", account_id)
+        account = Account.find(account_id)
+        if not account:
+            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        account.deserialize(request.get_json())
+        account.update()
+        return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+    def delete_accounts(account_id):
+        """
+        Delete an Account
+        This endpoint will delete an Account based on the account_id that is requested
+        """
+        app.logger.info("Request to delete an Account with id: %s", account_id)
+        account = Account.find(account_id)
+        if account:
+            account.delete()
+        return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
